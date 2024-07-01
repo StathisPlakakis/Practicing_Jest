160c209b280e099928fdf0953ec2d71c4c35196c
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-07-01 21:13:16 +0300

    Add test cases for reverseString function

--- functions.test.js
+++ functions.test.js
@@ -22,4 +22,24 @@ test('Capitalize function', () => {
   testCases.forEach(testCase => {
     expect(capitalize(testCase.received)).toBe(testCase.expected);
   })
-})
+});
+
+test('ReverseString function', () => {
+  const testCases = [
+    {
+      received: '',
+      expected: ''
+    },
+    {
+      received: 'Hello',
+      expected: 'olleH'
+    },
+    {
+      received: 'Hello World !,$@',
+      expected: '@$,! dlroW olleH'
+    }
+  ]
+  testCases.forEach(testCase => {
+    expect(reverseString(testCase.received).toBe(testCase.expected));
+  })
+});
